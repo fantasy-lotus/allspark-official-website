c3dfdf9407b2aa7755d28255fcf206b482418401
lotus <gilgamesh258@qq.com>
2025-01-15 19:46:41 +0800

    init

---
+++ src/main/java/com/allspark/allsparkofficialwebsite/AllsparkOfficialWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.allspark.allsparkofficialwebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllsparkOfficialWebsiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AllsparkOfficialWebsiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/allspark/allsparkofficialwebsite/AllsparkOfficialWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allspark.allsparkofficialwebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllsparkOfficialWebsiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
